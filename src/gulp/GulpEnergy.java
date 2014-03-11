7956be0f95d6947aaddf690aa3410028f64f5043
Will Tipton <wwt26@cornell.edu>
2014-03-11 15:32:47 -0400

    add GPL headers to all our source files

--- src/gulp/GulpEnergy.java
+++ src/gulp/GulpEnergy.java
@@ -1,4 +1,23 @@
-/* Genetic algorithm for crystal structure prediction.  Will Tipton.  Ceder Lab at MIT. Summer 2007 */
+/*
+ * Copyright 2011-2014 Will Tipton, Richard Hennig, Ben Revard, Stewart Wenner
+
+This file is part of the Genetic Algorithm for Structure and Phase Prediction (GASP).
+
+    GASP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GASP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GASP.  If not, see <http://www.gnu.org/licenses/>.
+    
+    
+    */
 
 package gulp;
 
