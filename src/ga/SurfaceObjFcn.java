462d1878828bbcd5d24eb856141b105f0661df01
Benjamin Revard <benjaminrevard@rrdhcp-10-32-158-29.redrover.cornell.edu>
2014-04-24 16:15:35 -0400

    Committing unimportant local changes (just used for testing) so I can pull.

--- src/ga/SurfaceObjFcn.java
+++ src/ga/SurfaceObjFcn.java
@@ -197,7 +197,7 @@ public class SurfaceObjFcn extends ObjectiveFunction {
 		
 		String arg[] = {"7", "hi"};
 		
-		StructureOrg c = new StructureOrg(VaspOut.getPOSCAR("/Users/benjaminrevard/GA/vasp/InP/ch5_initial_structs/8733.POSCAR"));
+		StructureOrg c = new StructureOrg(VaspOut.getPOSCAR("/Users/benjaminrevard/GA/vasp/InP/some_structures/tri_111.POSCAR"));
 		
 		// Convert array of strings to list of strings for the constructor
 		List<String> larg = Arrays.asList(arg);
@@ -210,11 +210,11 @@ public class SurfaceObjFcn extends ObjectiveFunction {
 		
 		cof.unpadOrg();
 		
-//		cof.padOrg();
+		cof.padOrg();
 		
-		c.getCell().writeCIF("/Users/benjaminrevard/GA/vasp/InP/ch5_initial_structs/8733.cif");
+		c.getCell().writeCIF("/Users/benjaminrevard/GA/vasp/InP/some_structures/tri_111.cif");
 				
-		(new VaspIn(Cell.parseCif(new File("/Users/benjaminrevard/GA/vasp/InP/ch5_initial_structs/8733.cif")), null, null, null)).writePoscar("/Users/benjaminrevard/GA/vasp/InP/ch5_initial_structs/8733unpadded.POSCAR", false);
+		(new VaspIn(Cell.parseCif(new File("/Users/benjaminrevard/GA/vasp/InP/some_structures/tri_111.cif")), null, null, null)).writePoscar("/Users/benjaminrevard/GA/vasp/InP/some_structures/tri_111padded.POSCAR", false);
 		
 //		System.out.println(GAParameters.getParams().getMinInteratomicDistance());
 
